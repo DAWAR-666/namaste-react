59c90da1622dd9266e257a4b48fdb33659e8cab4
DAWAR <dawarpratham8@gmail.com>
2025-11-23 16:48:49 +0530

    seperate file created

--- app.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom"
-const heading=React.createElement("h1",{},"sup");
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
